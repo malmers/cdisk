441612e3f4e01db116aac59f4334043314e32c89
Pontus Malm <malm@chalmers.it>
2016-03-02 11:48:09 +0100

    Mounts ram disk

--- ramdisk.c
+++ ramdisk.c
@@ -2,9 +2,30 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <ctype.h>
 
 #define GIGABYTE 2097152
 #define PATH_SIZE 100
+
+char *trimwhitespace(char *str) {
+  // http://stackoverflow.com/questions/122616/
+  char *end;
+
+  // Trim leading space
+  while(isspace(*str)) str++;
+
+  if(*str == 0)  // All spaces?
+    return str;
+
+  // Trim trailing space
+  end = str + strlen(str) - 1;
+  while(end > str && isspace(*end)) end--;
+
+  // Write new null terminator
+  *(end+1) = 0;
+
+  return str;
+}
 
 int main(int argc, char *argv[]) {
   char path[PATH_SIZE];
@@ -14,7 +35,7 @@ int main(int argc, char *argv[]) {
 
   if(argc < 2) {
     fprintf(stderr, "Too few arguments\n");
-  	return 1;
+    return 1;
   }
 
   size = strtol(argv[1], NULL, 10);
@@ -22,7 +43,7 @@ int main(int argc, char *argv[]) {
 
   if(pipe(fd) == -1) {
     perror("pipe");
-  exit(EXIT_FAILURE);
+    exit(EXIT_FAILURE);
   }
 
   pid = fork();
@@ -30,7 +51,6 @@ int main(int argc, char *argv[]) {
     fprintf(stderr, "Fork failed");
   } else if(pid == 0) {
     close(fd[0]);
-    printf("%s", ram);
     dup2(fd[1], fileno(stdout));
     execlp("hdiutil", "hdiutil", "attach", "-nomount", ram, NULL);
     exit(1);
@@ -41,11 +61,12 @@ int main(int argc, char *argv[]) {
     FILE *stream;
 
     stream = fdopen(fd[0], "r");
-    fread(path, 1, PATH_SIZE, stream);
+    fread(path, PATH_SIZE, 1, stream);
     fclose (stream);
-
-    printf("Path: \"%s\"\n", path);
+    trimwhitespace(path);
   }
+
+  execlp("diskutil", "diskutil", "erasevolume", "HFS+", "RAM Disk", path, NULL);
 
   return 0;
 }
